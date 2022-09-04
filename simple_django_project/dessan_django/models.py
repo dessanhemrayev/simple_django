4a73e9023a759e29ca27818440dc44e59692c05a
dessanhemrayev <dessanhemrayev@gmail.com>
2022-09-04 20:48:53 +0300

    added models

--- simple_django_project/dessan_django/models.py
+++ simple_django_project/dessan_django/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Hotel(models.Model):
+    name = models.CharField(max_length=256)
+    site_url = models.CharField(max_length=256)
+    count_rings = models.IntegerField(name = "Количество звезд")
+    count_rooms = models.IntegerField(name = "Количество комнат")
+        
+class Place(models.Model):
+    name = models.CharField(max_length=256)
+    is_open = models.BooleanField(default=True)
